2db9f31f80ed798d8520b9db8bbbdd49e5204913
renanbr <renandelima@gmail.com>
2018-01-18 04:00:54 +0100

    remove vestige of v2 code in tests

--- tests/Parser/CitationKeyTest.php
+++ tests/Parser/CitationKeyTest.php
@@ -35,9 +35,9 @@ class CitationKeyTest extends TestCase
         // 1 -> citation key
         // 2 -> original entry
         $this->assertCount(3, $listener->calls);
-        list($text, $type) = $listener->calls[1];
+        list($text, $context) = $listener->calls[1];
 
-        $this->assertSame('DBLP:journals/npl/CaamanoSBD16', $text);
+        $this->assertSame('DBLP:journals/npl/CaamanoSBD16', $context['state']);
         $this->assertSame(Parser::KEY, $type);
     }
 
@@ -59,9 +59,9 @@ class CitationKeyTest extends TestCase
         // 1 -> citation key
         // 2 -> original entry
         $this->assertCount(3, $listener->calls);
-        list($text, $type) = $listener->calls[1];
+        list($text, $context) = $listener->calls[1];
 
-        $this->assertSame('Kyriakakis:2016:EMI:3003733.3003777', $text);
+        $this->assertSame('Kyriakakis:2016:EMI:3003733.3003777', $context['state']);
         $this->assertSame(Parser::KEY, $type);
     }
 }
